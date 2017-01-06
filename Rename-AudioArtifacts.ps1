6c9950003349c79ede124bf90efe0139a75c9d83
Gregor Zurowski <gregor@zurowski.net>
2017-01-06 20:16:49 +0100

    Add support for sleeve images

--- Rename-AudioArtifacts.ps1
+++ Rename-AudioArtifacts.ps1
@@ -87,6 +87,9 @@ function RenameArtwork ([string]$prefix) {
         elseif ($file.Name -match "^obi\.(?<ext>$BitmapExt)$" -or $file.Name -match "_-_Artwork-Obi\.(?<ext>$BitmapExt)$") {
             $newName = GetArtworkFileName "Obi" $prefix $matches.ext
         }
+        elseif ($file.Name -match "^sleeve-(?<side>front|back)\.(?<ext>$BitmapExt)$") {
+            $newName = GetArtworkFileName ("Sleeve-" + $matches.side.Substring(0, 1).ToUpper() + $matches.side.Substring(1).ToLower()) $prefix $matches.ext
+        }
         elseif ($file.Name -eq "folder.jpg") {
             # do nothing
         }
